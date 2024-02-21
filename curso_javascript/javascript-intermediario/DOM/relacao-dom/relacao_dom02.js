0f2ff66dfcd023da3c56744d30526a9eff33a5f1
YgorHenriqueLima <118612433+YgorHenriqueLima@users.noreply.github.com>
2024-02-20 21:32:40 -0300

    estudando relação DOM adicionando elementos child

---
+++ curso_javascript/javascript-intermediario/DOM/relacao-dom/relacao_dom02.js
@@ -0,0 +1,6 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+
+const novoElemento = document.createElement('div')
+
+caixa1.appendChild(novoElemento)
