b9a0b84ffb3245ef00211cbfa108b85b190c9d3b
YgorHenriqueLima <118612433+YgorHenriqueLima@users.noreply.github.com>
2024-03-28 22:14:03 -0300

    terminando 2 desafios

---
+++ templates/desafio01/sons.js
@@ -0,0 +1,14 @@
+export default [
+    {
+      nome: 'speed-of-sound',
+      src: './audio/speed-of-sound.mp3'
+    },
+    {
+      nome: 'a-sky-of-Stars',
+      src: './audio/a-sky-full-of-Stars.mp3'
+    },
+    {
+      titulo: 'adventure_of_a_life',
+      src: './audio/adventure_of_a_life.mp3'
+    }
+];
