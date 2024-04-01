1805785b2d609643e88511f2e72fc78dbdb4c112
YgorHenriqueLima <118612433+YgorHenriqueLima@users.noreply.github.com>
2024-04-01 20:00:54 -0300

    alterando...

--- templates/desafio01/sons.js
+++ templates/desafio01/sons.js
@@ -1,14 +1,14 @@
 export default [
     {
-      nome: 'speed-of-sound',
+      nome: 'speed of sound',
       src: './audio/speed-of-sound.mp3'
     },
     {
-      nome: 'a-sky-of-Stars',
+      nome: 'a sky of Stars',
       src: './audio/a-sky-full-of-Stars.mp3'
     },
     {
-      titulo: 'adventure_of_a_life',
+      nome: 'adventure of a life',
       src: './audio/adventure_of_a_life.mp3'
     }
 ];
